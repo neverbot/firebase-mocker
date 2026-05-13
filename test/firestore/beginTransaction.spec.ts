c67c2139535e5608f815f4b28af03f8ab920f23e
neverbot <ivan@neverbot.com>
2026-05-13 11:53:27 +0200

    test(firestore): cover beginTransaction error paths to reach 100%

--- test/firestore/beginTransaction.spec.ts
+++ test/firestore/beginTransaction.spec.ts
@@ -34,4 +34,61 @@ describe('Firestore BeginTransaction (unit)', () => {
 
     handleBeginTransaction(server, call, callback);
   });
+
+  it('returns INTERNAL when createTransaction throws an Error', function (done) {
+    const fakeServer = {
+      getStorage: () => ({
+        createTransaction: () => {
+          throw new Error('boom');
+        },
+      }),
+      logger: { log: () => {} },
+    } as unknown as Parameters<typeof handleBeginTransaction>[0];
+
+    const call = { request: {} } as grpc.ServerUnaryCall<any, any>;
+
+    const callback: grpc.sendUnaryData<any> = (err, value) => {
+      try {
+        expect(value).to.be.undefined;
+        expect(err).to.exist;
+        const e = err as grpc.ServiceError;
+        expect(e.code).to.equal(grpc.status.INTERNAL);
+        expect(e.message).to.equal('boom');
+        done();
+      } catch (assertionErr) {
+        done(assertionErr);
+      }
+    };
+
+    handleBeginTransaction(fakeServer, call, callback);
+  });
+
+  it('returns INTERNAL with "Unknown error" when createTransaction throws a non-Error', function (done) {
+    const fakeServer = {
+      getStorage: () => ({
+        createTransaction: () => {
+          // eslint-disable-next-line @typescript-eslint/only-throw-error
+          throw 'string-thrown';
+        },
+      }),
+      logger: { log: () => {} },
+    } as unknown as Parameters<typeof handleBeginTransaction>[0];
+
+    const call = { request: {} } as grpc.ServerUnaryCall<any, any>;
+
+    const callback: grpc.sendUnaryData<any> = (err, value) => {
+      try {
+        expect(value).to.be.undefined;
+        expect(err).to.exist;
+        const e = err as grpc.ServiceError;
+        expect(e.code).to.equal(grpc.status.INTERNAL);
+        expect(e.message).to.equal('Unknown error');
+        done();
+      } catch (assertionErr) {
+        done(assertionErr);
+      }
+    };
+
+    handleBeginTransaction(fakeServer, call, callback);
+  });
 });
